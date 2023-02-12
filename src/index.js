d855709bb2f4ca504c79423c8c72f62760c4b000
Wes Basinger <dbumathlete@gmail.com>
2023-02-12 14:56:50 -0600

    Fully draggable 5x5 yellow rectangle on a 25 px grid

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 import bgImg from './assets/grid.png';
 
+const gameState = {}
+
 class Basic extends Phaser.Scene
 {
     constructor ()
@@ -15,8 +17,20 @@ class Basic extends Phaser.Scene
       
     create ()
     {
-        const bg = this.add.image(400, 300, 'bg');
-      
+        this.add.image(412.5, 312.5, 'bg');
+
+        gameState.r1 = this.add.rectangle(125,125, 125,125, 0xFFFF00);
+
+        gameState.r1.setInteractive();
+
+        this.input.setDraggable(gameState.r1);
+
+        this.input.on('drag', function(pointer, gameObj, dragX, dragY) {
+            let gridSize = 25;
+            gameObj.x = Phaser.Math.Snap.To(dragX, gridSize);
+            gameObj.y = Phaser.Math.Snap.To(dragY, gridSize);
+        })
+
     }
 }
 
