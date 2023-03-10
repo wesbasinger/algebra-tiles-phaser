a70b0d014ebe2571d9013976698793f899ceec4a
Wes Basinger <dbumathlete@gmail.com>
2023-03-09 20:53:15 -0600

    Trying something different

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Phaser from 'phaser';
 
+import oneByOne from './assets/1x1.png'
+
 const gameState = {}
 
 class Basic extends Phaser.Scene
@@ -8,49 +10,32 @@ class Basic extends Phaser.Scene
     {
         super();
     }
+
+    preload()
+    {
+        this.load.image('1x1', oneByOne);
+    }
       
     create ()
     {
 
-        let rectangle1 = this.add.rectangle(100, 100, 50, 50, 0xff0000).setInteractive();
-        let rectangle2 = this.add.rectangle(200, 200, 50, 50, 0x00ff00).setInteractive();
+        let rectangle1 = this.physics.add.sprite(100, 100, '1x1').setInteractive();
+        let rectangle2 = this.physics.add.sprite(200, 200, '1x1').setInteractive();
 
-        let staticRectangle = rectangle1; // set rectangle1 as the static rectangle
+        rectangle1.setPushable(true)
+        rectangle2.setPushable(true);
 
-        this.input.setDraggable([rectangle2]);
+        this.input.setDraggable(rectangle1);
+        this.input.setDraggable(rectangle2);
 
         this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-            if (gameObject !== staticRectangle) { // check if the dragged rectangle is not the static rectangle
-                gameObject.x = dragX;
-                gameObject.y = dragY;
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        })
 
-                // check for collision between the dragged rectangle and the static rectangle
-                if (Phaser.Geom.Intersects.RectangleToRectangle(gameObject.getBounds(), staticRectangle.getBounds())) {
-                    
-                    // calculate the distance between the two rectangles
-                    let distanceX = Math.abs(gameObject.x - staticRectangle.x);
-                    let distanceY = Math.abs(gameObject.y - staticRectangle.y);
-                    
-                    // move the dragged rectangle apart from the static rectangle along the shortest distance
-                    if (distanceX > distanceY) {
-                        if (gameObject.x > staticRectangle.x) {
-                            gameObject.x += distanceX/2;
-                        } else {
-                            gameObject.x -= distanceX/2;
-                        }
-                    } else {
-                        if (gameObject.y > staticRectangle.y) {
-                            gameObject.y += distanceY/2;
-                        } else {
-                            gameObject.y -= distanceY/2;
-                        }
-                    }
-                }
-            } else { // if the dragged rectangle is the static rectangle, reset its position to its original position
-                gameObject.x = gameObject.input.dragStartX;
-                gameObject.y = gameObject.input.dragStartY;
-            }
-});
+        this.physics.add.collider(rectangle1, rectangle2, () => {
+            console.log("Heard collision");
+        })
 
 
     }
